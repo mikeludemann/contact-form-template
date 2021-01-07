a7a4c50a37b850888b7b8d05d772e76cbf658fac
Mike Ludemann <mike-ludemann@hotmail.de>
2021-01-07 14:38:55 +0100

    Setup complete - contact-form-template

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # contact-form-template
+
 A simple contact form template
+
+## Installation & Help
+
+### Installation
+
+#### Voraussetzungen
+
+Webserver mit PHP Unterstützung (mind. PHP 7.0, PHP 8.0 ready)
+Editor zum bearbeiten der Dateien
+
+#### Anleitung
+
+1. TBD
---
+++ danke.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="de-DE">
+	<head>
+		<meta charset="utf-8">
+		<meta name="language" content="de"/>
+		<meta name="description" content="Kontakt"/>
+		<meta name="revisit" content="After 7 days"/>
+		<meta name="robots" content="INDEX,FOLLOW"/>
+		<title>Kontakt</title>
+	
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+<style type="text/css">
+body{font-family:Verdana,Arial,Helvetica,sans-serif;font-size:14px;color:#000000;background-color: #FFFFFF;}
+a:link, a:visited, a:active{color:#000000;text-decoration:none;}
+a:hover{text-decoration: underline;}
+</style>
+
+	</head>
+
+
+
+<body>
+
+<div>
+
+ 
+    
+	<p>Vielen Dank. Ihre Nachricht wurde erfolgreich &uuml;bermittelt.</p>		
+ <p><br /><br />
+ <a href="https://www.example.com" onclick="window.open(this.href); return false;" title="Kontakt" style="font-size:12px; ">&copy; by Admin</a></p>
+
+</div>
+
+</body>
+</html>
